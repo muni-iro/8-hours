4c85c9d1729bc382fe25f28208b9de654b8f29df
muni-iro <69463789+muni-iro@users.noreply.github.com>
2020-08-11 13:18:41 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+
+$(".btn-trigger").on("click",function(){
+	$(".side-ber").addClass("active");
+});
+
+$(".btn-close").on("click",function(){
+	$(".side-ber").removeClass("active");
+});
